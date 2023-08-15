4bec1a52f4f27c5c502139ba290e926ecc4b66de
NanoCode10 <marianoars10@gmail.com>
2023-08-15 15:33:52 -0300

    second docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async(category) => {
 
-    const url = `https:api.giphy.com/v1/gifs/search?api_key=2Aepd2bOqqOHfGkEG1CbudlqVW1nUpr5&q=${ category }&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=2Aepd2bOqqOHfGkEG1CbudlqVW1nUpr5&q=${ category }&limit=10`
     const resp = await fetch( url )
     const { data } = await resp.json()
     
